546682d824d41436cf80e40ea8d0bbd61dc485d1
Emre <sjoxlyses7@gmail.com>
2025-06-30 14:41:47 +0300

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// replace with your repo name
-export default defineConfig({
-  base: '/myblog/', // Örn: '/blog-projem/'
-  plugins: [react()],
-})
